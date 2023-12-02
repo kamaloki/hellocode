aa638e69ab31f0d93ac983c345507579f45e6df1
Kamila <89898648749@mail.ru>
2023-12-02 20:11:01 +0300

    add exaple 005 Conditial

---
+++ example005_Conditial/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Камила"){
+    Console.WriteLine("Урааа, это же КАМИЛА!");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
